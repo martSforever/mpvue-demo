05a1fc9dd5d5ec085d75dc2cf06b1bdd7882690b
weishengjian <shengjian.wei@hand-china.com>
2018-06-19 20:58:44 +0800

    格式化豆瓣查询结果

--- server/modules/book/controller/book.controller.js
+++ server/modules/book/controller/book.controller.js
@@ -9,7 +9,20 @@ let {get} = require("../../../app/script/http");
 async function addBookPost(ctx, next) {
   let {isbn, openId} = ctx.request.body;
   if (!!isbn && !!openId) {
-    ctx.state.data = await get(`https://api.douban.com/v2/book/isbn/${isbn}`);
+
+    let bookInfo = await get(`https://api.douban.com/v2/book/isbn/${isbn}`);
+    let rate = bookInfo.rating.average;
+    let {title, image, alt, publisher, summary, price} = bookInfo;
+    let tags = bookInfo.tags.map(v => {
+      return `${v.title} ${v.count}`;
+    }).join(',');
+    let authors = bookInfo.author.join(',');
+    let ret = {
+      rate, title, image, alt, publisher, summary, price, tags, authors
+    }
+    console.log(ret);
+
+    ctx.state.data = bookInfo;
   } else {
     ctx.state.code = 1;
   }
