c42232f76e8ae30c6a93b335efb393a43952e2f6
weishengjian <shengjian.wei@hand-china.com>
2018-06-19 14:32:16 +0800

    消息提示示例页面抽离出来

--- src/pages/books/Books.vue
+++ src/pages/books/Books.vue
@@ -1,20 +1,12 @@
 <template>
   <div>
     <div>图书列表界面</div>
-    <input v-model="message"/>
-    <button @click="toast">toast</button>
-    <button @click="loading">loading</button>
-    <button @click="modal">modal</button>
-    <button @click="confirm">confirm</button>
-    <button @click="actionSheet">actionSheet</button>
   </div>
 </template>
 
 <script>
-  import {hideLoading, showActionSheet, showConfirm, showLoading, showModal, showToast} from "../../base/script/msg";
-
   export default {
-    name: "me",
+    name: "books",
     data() {
       return {
         message: '确定要提交记录吗？'
@@ -23,38 +15,7 @@
     created() {
     },
     methods: {
-      toast() {
-        showToast(this.message, {image: '/static/img/book-active.png'});
-      },
-      loading() {
-        showLoading('loading haha...');
-        setTimeout(() => {
-          hideLoading();
-        }, 5000)
-      },
-      modal() {
-        showModal(this.message, {
-          success(ret) {
-            console.log('complete', ret);
-          },
-        });
-      },
-      confirm() {
-        showConfirm(this.message, {
-          onConfirm() {
-            console.log('handle onConfirm');
-          }, onCancel() {
-            console.log('handle onCancel');
-          },
-        })
-      },
-      actionSheet() {
-        showActionSheet(['菜单一', '菜单二'], {
-          success(ret){
-            console.log(ret);
-          },
-        })
-      },
+
     }
   }
 </script>
---
+++ src/pages/example/msg/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Msg from './Msg'
+
+const msg = new Vue(Msg);
+msg.$mount();
+
+export default {}
