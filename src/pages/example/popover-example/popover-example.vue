a38417feab32ab6f9180c87822dfff769bc99857
martSforever <qq450881199>
2018-07-03 15:27:23 +0800

    弹出框示例增加打开以及关闭按钮

--- src/pages/example/popover-example/popover-example.vue
+++ src/pages/example/popover-example/popover-example.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="dev-wrapper">
     <lnk-switch v-model="isChecked"/>
+    <div class="open-btn-wrapper">
+      <div class="open-btn" @click="openPopover">oo</div>
+    </div>
     <div class="popover-wrapper" :style="popoverWrapperStyles" @click="closePopover">
+      <div class="close-btn-wrapper">
+        <div class="close-btn" @click="openPopover">cc</div>
+      </div>
       <div class="popover-content" style="width: 500rpx;" @click.stop="handleClickPopoverContent">
         <div @click="sayHello" class="btn">sayHello</div>
       </div>
@@ -10,6 +16,7 @@
 </template>
 
 <script>
+
 
 
   import LnkSwitch from "../../../base/base-component/lnk-switch/lnk-switch";
@@ -34,6 +41,9 @@
       handleClickPopoverContent() {
 
       },
+      openPopover() {
+        this.isChecked = true;
+      },
     },
     computed: {
       popoverWrapperStyles() {
@@ -48,6 +58,22 @@
 
 <style scoped lang="scss">
   /*@formatter:off*/
+  .open-btn-wrapper{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    .open-btn{
+      height: 200rpx;
+      box-sizing: border-box;
+      display: flex;
+      align-items: center;
+      background-color: brown;
+      color: white;
+    }
+  }
   .popover-wrapper{
     position: absolute;
     top: 0;
@@ -55,10 +81,23 @@
     width: 100%;
     background-color: rgba(black,0.3);
     transition: all 0.4s;
+    display: flex;
+    justify-content: flex-end;
+    .close-btn-wrapper{
+      height: 100%;
+      display: inline-flex;
+      align-items: center;
+      .close-btn{
+        height: 200rpx;
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        background-color: brown;
+        color: white;
+      }
+    }
     .popover-content{
       height: 100%;
-      position: absolute;
-      right: 0;
       background-color: white;
     }
     .btn{
