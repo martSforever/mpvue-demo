256b4641ea4ae180554442cc9aa7c1f340fe2dcf
martSforever <qq450881199>
2018-07-04 13:33:44 +0800

    在tabbar页面测试了触底加载，好像是不能实现每个页面单独触底加载，但是可以通过scroll-view这个小程序组件实现滚动到底部的时候，自动加载；现在准备实现一个lnk-list类似的功能，只能实现数据自动加载，触底自动加载下一页数据，，无法实现下拉刷新的功能

--- src/pages/example/tabbar-example/page1.vue
+++ src/pages/example/tabbar-example/page1.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
+  <div class="page-wrapper">
     <div>this is page1</div>
     <div><input v-model="val"/></div>
+    <scroll-view class="list-wrapper" :scroll-y="true" @scrolltolower="handleScrolltolower">
+      <div class="list-item" v-for="(item,index) in [1,2,3,4,5]" :key="index">
+        {{index}}
+      </div>
+    </scroll-view>
   </div>
 </template>
 
@@ -13,8 +18,13 @@
         val: 'ssda'
       }
     },
+    methods:{
+      handleScrolltolower(){
+          console.log('handleScrolltolower')
+      },
+    },
     onLoad() {
-      console.log('page1 onload...',this.$nav.currentPageParam);
+      console.log('page1 onload...', this.$nav.currentPageParam);
     },
     mounted() {
       console.log('page1 mounted...');
@@ -22,9 +32,29 @@
     created() {
       console.log('page1 created...');
     },
+    onReachBottom() {
+      console.log('onReachBottom....');
+    },
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  /*@formatter:off*/
+.page-wrapper{
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  .list-wrapper{
+    flex: 1;
+    overflow: scroll;
+    .list-item{
+      height: 300px;
+      background-color: brown;
+      color: white;
+      margin-bottom: 12px;
+    }
+  }
+}
+/*@formatter:on*/
 </style>
