69b11e9644e10248a857efbae28ebd65050b12af
martSforever <qq450881199>
2018-07-04 10:54:36 +0800

    吧测试插槽的代码保存下来，先测试多个tab页面触底加载的功能

---
+++ src/pages/example/test-slot/test-slot-child.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      this is child head...
+    </div>
+    <slot name="listContent"></slot>
+    <div>
+      this is child foot...
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "test-slot-child"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/example/test-slot/test-slot-main.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="dev-wrapper">
+    <div>
+      this is test-slot-main
+    </div>
+    <test-slot-child>
+      <template slot="listContent">
+        <div class="list-content">
+          this is parent content
+        </div>
+      </template>
+    </test-slot-child>
+  </div>
+</template>
+<script>
+
+
+  import TestSlotChild from "./test-slot-child";
+
+  export default {
+    components: {TestSlotChild},
+    name: "test-slot-main",
+    data() {
+      return {
+        isChecked: false,
+      }
+    },
+    methods: {},
+    computed: {},
+  }
+</script>
+
+<style scoped lang="scss">
+  /*@formatter:off*/
+  .list-content{
+    height: 300px;
+    background-color: brown;
+    color: white;
+  }
+  /*@formatter:on*/
+</style>
